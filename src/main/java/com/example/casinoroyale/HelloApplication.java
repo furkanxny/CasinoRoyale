aba22aaadd0a8f47d0fe96a49a0e2af912719f48
luke-angelostrazzera <lukestrazzera@gmail.com>
2023-09-05 14:30:59 -0400

    CS-000 create scene builder

--- src/main/java/com/example/casinoroyale/HelloApplication.java
+++ src/main/java/com/example/casinoroyale/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello world!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("SceneBuilder.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 3200, 2400);
+        stage.setTitle("Casino Royale");
         stage.setScene(scene);
         stage.show();
     }
