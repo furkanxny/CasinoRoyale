98ad788bc4a495532ad69a9d4ab82f0f4aa375ef
luke-angelostrazzera <lukestrazzera@gmail.com>
2023-09-19 18:58:13 -0400

    wip

--- src/main/java/app/casinoroyale/CSRApplication.java
+++ src/main/java/app/casinoroyale/CSRApplication.java
@@ -3,30 +3,35 @@ package app.casinoroyale;
 import app.casinoroyale.Controller.HomeController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class CSRApplication extends Application {
     private static Stage stage;
+
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        this.stage = primaryStage;
-        VBox root = new VBox();
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/app.casinoroyale/View/Games/Roulette.fxml"));
-        fxmlLoader.setRoot(root);
-        fxmlLoader.load();
+        // removes the top bar of the application window. (undecorated stage)
+        stage.initStyle(StageStyle.TRANSPARENT);
 
-        HomeController controller = fxmlLoader.getController();
-        controller.setStage(primaryStage);
+        Parent root = FXMLLoader.load(getClass().getResource("/app.casinoroyale/View/Games/Roulette.fxml"));
 
-        Scene scene = new Scene(root, 3200, 2400);
-        primaryStage.setTitle("Casino Royale");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.setTitle("Casino");
+        stage.setScene(scene);
+        stage.show();
+
+
     }
 
 
