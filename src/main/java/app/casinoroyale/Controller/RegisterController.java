e311a03fec9ecb4c8330deb20f99f5fc151254da
furkan uzun <uzunf@farmingdale.edu>
2023-11-18 09:23:58 -0500

    LoginController and RegisterController has been added with their .fxml files

---
+++ src/main/java/app/casinoroyale/Controller/RegisterController.java
@@ -0,0 +1,38 @@
+package app.casinoroyale.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+    interface regex{
+        String regexUserName = "\\b[A-Z][a-zA-Z]+";
+        String regexEmail = "[a-z0-9]+@[a-z0-9]+.[0-z]{2,6}";
+        String regexPassword =
+                "  ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$\n";
+
+}
+public class RegisterController {
+
+    @FXML
+    private TextField userNameTF;
+    @FXML
+    private TextField emailTF;
+    @FXML
+    private TextField password1TF;
+    @FXML
+    private TextField password2TF;
+    @FXML
+    private RadioButton terms;
+
+    private String userName;
+    private String email;
+    private String password;
+    private String passwordConfirmation;
+
+    public void registerButtonHandler(ActionEvent event) {
+    }
+
+    public void signInButtonHandler(ActionEvent event) {
+    }
+}
