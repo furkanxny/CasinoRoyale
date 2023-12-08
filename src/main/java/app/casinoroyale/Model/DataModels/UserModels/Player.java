5a09977f84c4063c061be0ce9fc1cb737a052514
cmariana24 <mariac2@rpi.edu>
2023-12-08 18:15:49 -0500

    Player class copied from 000 on git

--- src/main/java/app/casinoroyale/Model/DataModels/UserModels/Player.java
+++ src/main/java/app/casinoroyale/Model/DataModels/UserModels/Player.java
@@ -1,78 +1,103 @@
-/*  
+/*
  *  © Copyright yannickhuggler.com
  *
  *  [Project Title]     Roulette
  *  [Description]       The king of all casino-games implemented in JavaFX.
  *  [Authors]           Yannick Huggler
- *  [Version]           Version 1.0      
+ *  [Version]           Version 1.0
  */
 
 package app.casinoroyale.Model.DataModels.UserModels;
 
+
+import app.casinoroyale.Model.DataModels.GameModels.BlackJackModel.deck.Deck;
+import app.casinoroyale.Model.DataModels.GameModels.BlackJackModel.game.Chip;
+import app.casinoroyale.Model.DataModels.GameModels.BlackJackModel.role.Cardholder;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import app.casinoroyale.Controller.FirebaseControllers.Person;
 
-public class Player extends Person {
+public class Player extends Cardholder {
+
     public static final Person person = null;
     private static Player instance = null;
-   
+    private double bet;
+
+    public String name;
     private double accountBalance;
-    
-    
-    /**
-     * Private constructor due to the use of the singleton-pattern.
-     */
 
     public Player() {
         super();
     }
 
-    /**
-     * If there is no player-object, it gets created, otherwise it just returns the aforementioned object.
-     * @return Player object
-     */
     public static Player getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new Player();
         }
         return instance;
     }
-    
-   
 
+    public void reset() {
+        this.bet = 0;
+        this.clearHand();
+        // Reset any other game-specific information in the Player class
+    }
+
+
+    public void deal(ArrayList<Chip> chips) {
+        bet = 0; // Reset the bet amount
+        chips.forEach(chip -> bet += chip.getValue());
+        accountBalance -= bet;
+    }
+
+
+    public void doubleDeal() {
+        bet *= 2;
+        accountBalance -= bet; // Deduct the adjusted bet amount
+    }
+
+
+    public String getName(){return name;}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void hit(Deck deck) {
+        takeCard(deck);
+    }
+
+    public void setBalance(double balance) {
+        this.accountBalance = balance;
+    }
+
+    public void clearBet() {
+        bet = 0;
+    }
+
+    public double getBet() {
+        return bet;
+    }
+
+    public double getBalance() {
+        return accountBalance;
+    }
+
+    public String getAccountBalanceFormatted() {
+        DecimalFormat formatter = new DecimalFormat("\t$###,###");
+        return formatter.format(accountBalance);
+    }
+    public void setAccountBalanceFromFirebase(Double balance){
+        this.accountBalance  = balance;
+    }
     public double getAccountBalance() {
         return accountBalance;
     }
-    
-    /**
-     * Adds the difference of balance to the player's account balance.
-     * @param difference 
-     */
-    public void setAccountBalance(double difference) {
-        this.accountBalance += difference;
+
+
+    public void setAccountBalance(double balance) {
+        this.accountBalance += balance;
     }
 
-    /**
-     * Checks if the player can withdraw the specified amount.
-     * @param amount The amount to check for withdrawal.
-     * @return true if withdrawal is possible, false otherwise.
-     */
-    public boolean canWithdraw(double amount) {
-        return amount > 0 && this.accountBalance >= amount;
-    }
 
-    /**
-     * Withdraws the specified amount from the player's account balance.
-     * @param amount The amount to withdraw.
-     */
-    public void withdraw(double amount) {
-        if (canWithdraw(amount)) {
-            setAccountBalance(-amount); // Negative value to deduct from balance
-        } else {
-            // Handle the case where withdrawal is not possible
-            // This could be throwing an exception or logging an error
-            throw new IllegalArgumentException("Cannot withdraw the specified amount. Insufficient funds or invalid amount.");
-        }
-    }
-    
-    
 }
